4b82eefcf576ee677bc77d6b8e3363704b80c63f
1219650953@qq.com <cj13032716642>
2020-06-23 11:46:53 +0800

    1

---
+++ src/main/java/com/xiangxue/thread/my/ch1/VolatileOneWriteMultiRead.java
@@ -0,0 +1,72 @@
+package com.xiangxue.thread.my.ch1;
+
+import com.xiangxue.tools.SleepTools;
+
+/**
+ * 测试Volatile一个线程写，多个线程读
+ */
+public class VolatileOneWriteMultiRead {
+    private volatile int a = 1;
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    //   写线程
+    private static class WriteThread extends Thread {
+        private VolatileOneWriteMultiRead volatileOneWriteMultiRead;
+
+        public WriteThread(String name, VolatileOneWriteMultiRead volatileOneWriteMultiRead) {
+            super(name);
+            this.volatileOneWriteMultiRead = volatileOneWriteMultiRead;
+        }
+
+        @Override
+        public void run() {
+            while(true){
+                long id = Thread.currentThread().getId();
+                int a = volatileOneWriteMultiRead.getA();
+                volatileOneWriteMultiRead.setA(++a);
+                System.out.println("写线程id" + id + "写入值a为" + volatileOneWriteMultiRead.getA());
+                SleepTools.second(2);
+            }
+
+
+        }
+    }
+
+    //    读线程
+    private static class ReadThread extends Thread {
+        private VolatileOneWriteMultiRead volatileOneWriteMultiRead;
+
+        public ReadThread(String name, VolatileOneWriteMultiRead volatileOneWriteMultiRead) {
+            super(name);
+            this.volatileOneWriteMultiRead = volatileOneWriteMultiRead;
+        }
+
+        @Override
+        public void run() {
+            long id = Thread.currentThread().getId();
+            System.out.println("读线程id" + id + "当前值a为" + volatileOneWriteMultiRead.getA());
+        }
+    }
+
+    public static void main(String[] args) {
+        VolatileOneWriteMultiRead volatileOneWriteMultiRead = new VolatileOneWriteMultiRead();
+        WriteThread writeThread = new WriteThread("我是写线程", volatileOneWriteMultiRead);
+        writeThread.start();
+        ReadThread readThread = new ReadThread("我是读线程", volatileOneWriteMultiRead);
+        ReadThread readThread2 = new ReadThread("我是读线程", volatileOneWriteMultiRead);
+        ReadThread readThread3 = new ReadThread("我是读线程", volatileOneWriteMultiRead);
+        ReadThread readThread4 = new ReadThread("我是读线程", volatileOneWriteMultiRead);
+        readThread.start();
+        readThread2.start();
+        readThread3.start();
+        readThread4.start();
+
+    }
+}
