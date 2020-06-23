bb16597a1130a283a1b063198b3b69bbf2017eca
1219650953@qq.com <cj13032716642>
2020-06-23 12:03:02 +0800

    1

--- src/main/java/com/xiangxue/thread/my/ch1/VolatileOneWriteMultiRead.java
+++ src/main/java/com/xiangxue/thread/my/ch1/VolatileOneWriteMultiRead.java
@@ -28,6 +28,7 @@ public class VolatileOneWriteMultiRead {
         @Override
         public void run() {
             while(true){
+                SleepTools.second(2);//此处等待2秒，让所有线程都初始化完成，不然会出现第一轮数据读线程可能无法取到
                 long id = Thread.currentThread().getId();
                 int a = volatileOneWriteMultiRead.getA();
                 synchronized (volatileOneWriteMultiRead){
@@ -35,9 +36,6 @@ public class VolatileOneWriteMultiRead {
                     System.out.println("写线程id" + id + "写入值a为" + volatileOneWriteMultiRead.getA());
                     volatileOneWriteMultiRead.notifyAll();
                 }
-                SleepTools.second(2);
-
-
 
             }
 
